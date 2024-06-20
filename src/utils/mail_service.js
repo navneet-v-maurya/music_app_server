6e574a7007b1cd0d247bc0da4c2606613eaba736
navneet <navneet.v.maurya@gmail.com>
2024-06-21 00:09:52 +0530

    project setup

---
+++ src/utils/mail_service.js
@@ -0,0 +1,10 @@
+//module imports
+import nodemailer from "nodemailer";
+
+export const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASS,
+  },
+});
