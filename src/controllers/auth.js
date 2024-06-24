ea51c2ab56f65ec81f53b5f546e893a2763d5da1
navneet <navneet.v.maurya@gmail.com>
2024-06-25 00:00:27 +0530

    added sign in by otp

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -295,3 +295,147 @@ export const sign_in = async (data, cb) => {
     );
   }
 };
+
+export const generate_otp_sign_in = async (data, cb) => {
+  try {
+    if (!data.unique_id) throw new Error("Params Missing");
+
+    const user_data = {};
+
+    if (email_regex.test(data.unique_id)) {
+      user_data.email = data.unique_id;
+    } else if (phone_number_regex.test(data.unique_id)) {
+      user_data.phone_number = data.unique_id;
+    } else {
+      throw new Error("Invalid Phone or Email");
+    }
+
+    const found = await User.findOne({
+      where: { [Op.or]: [{ email: data.unique_id }, { phone_number: data.unique_id }] },
+    });
+
+    if (!found) throw new Error("Email or Phone Not Found");
+
+    const token = create_token(user_data);
+
+    const otp = generate_otp(6);
+
+    const hashed_otp = await encrypt(otp);
+
+    const new_otp = new Otp({
+      otp: hashed_otp,
+      token,
+      unique_id: data.unique_id,
+    });
+
+    const promises = [new_otp.save()];
+
+    const message = `Dear ${user_data.name},
+
+Your OTP for Chord Sync Hub is [${otp}]. It is valid for 10 minutes. Please do not share this code with anyone.
+
+Thank you,
+Chord Sync Hub Team`;
+
+    if (user_data.email) {
+      const subject = "Your Chord Sync Hub OTP Code";
+      promises.push(send_email([user_data.email], subject, message));
+    } else if (user_data.phone_number) {
+      promises.push(send_message_to_phone(user_data.phone_number, message));
+    }
+
+    await Promise.all(promises);
+
+    return cb(
+      null,
+      response_structure
+        .merge({
+          success: true,
+          status: 200,
+          action: "sign_in",
+          message: "User Logged In Successfully",
+          data: token,
+        })
+        .toJS()
+    );
+  } catch (err) {
+    console.log(err);
+    return cb(
+      response_structure
+        .merge({
+          success: false,
+          status: 400,
+          action: "sign_in",
+          message: err.message,
+        })
+        .toJS()
+    );
+  }
+};
+
+export const sign_in_by_otp = async (data, cb) => {
+  try {
+    if (!data.token || !data.otp) throw new Error("Params Missing");
+
+    const user_data = decode_token(data.token);
+
+    if (!user_data) throw new Error("Invalid Token");
+
+    const found_otp = await Otp.findOne({
+      unique_id: user_data.email ? user_data.email : user_data.phone_number,
+      is_used: false,
+    }).sort({ updated_at: -1 });
+
+    if (!found_otp) throw new Error("No OTP Found");
+
+    const otp_matched = await compare(data.otp, found_otp.otp);
+
+    if (!otp_matched) throw new Error("Wrong OTP");
+
+    const [user, updated_otp] = await Promise.all([
+      User.findOne({ where: user_data }),
+      Otp.findByIdAndUpdate(found_otp._id, { is_used: true }),
+    ]);
+
+    const { password, ...rest } = user.dataValues;
+
+    const token = create_token({
+      _id: rest.id,
+      name: rest.name,
+      email: rest.email,
+      age: rest.age,
+      gender: rest.gender,
+      phone_number: rest.phone_number,
+      is_admin: rest.is_admin,
+      is_active: rest.is_active,
+    });
+
+    return cb(
+      null,
+      response_structure
+        .merge({
+          success: true,
+          status: 200,
+          action: "sign_in",
+          message: "User Logged In Successfully",
+          data: {
+            token,
+            user: rest,
+          },
+        })
+        .toJS()
+    );
+  } catch (err) {
+    console.log(err);
+    return cb(
+      response_structure
+        .merge({
+          success: false,
+          status: 400,
+          action: "sign_in",
+          message: err.message,
+        })
+        .toJS()
+    );
+  }
+};
